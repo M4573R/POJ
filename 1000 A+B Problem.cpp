7ad92e5a99f7cdd3ff2528db59a3b13cc8417d68
fuwutu <fuwutu@qq.com>
2013-08-02 21:38:55 +0800

    Create 1000 A+B Problem.cpp

---
+++ 1000 A+B Problem.cpp
@@ -0,0 +1,12 @@
+//11907672  fuwutu	1000	Accepted	204K	0MS	C++	143B	2013-08-02 21:37:01
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
